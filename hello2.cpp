b63bed88887a8b310da7df0a2be6da561cadf3a7
Mamba1099 <mambakaranja240@gmail.com>
2023-09-22 14:29:40 +0300

    function in c++

---
+++ hello2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+void my_function() {
+    cout << "Hello, World!" << endl;
+    cout << "Goodbye, World!";
+}
+
+int main() {
+    my_function();
+    return 0;
+}
+
