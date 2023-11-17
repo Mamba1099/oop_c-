bcab62184a3e6a4349877868ef0ce9fc28889ad4
Mamba1099 <mambakaranja240@gmail.com>
2023-11-17 16:19:06 +0300

    learning c++

---
+++ mamba/hello2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+void my_function() {
+    std::cout << "Hello, World!" <<std::endl;
+    std::cout << "Goodbye, World!";
+}
+
+int main() {
+    my_function();
+    return 0;
+}
+
