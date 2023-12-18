cd6600424cc7332223878defbe93cc3f9e7b08a6
Mamba1099 <mambakaranja240@gmail.com>
2023-12-18 23:22:53 +0300

    bool check

--- mamba/cin.cpp
+++ mamba/cin.cpp
@@ -9,7 +9,21 @@ void my_number() {
     cin >> num; // Get the input
     cout << "Your number is: " << num; // Display the input
 }
+void my_logic() {
+    int num1;
+    int num2;
+    cout << "Enter two numbers: ";
+    cin >> num1 >> num2;
+    // if (num1 > num2) {
+    //     cout << "The larger number is: " << num1;
+    // } else {
+    //     cout << "The larger number is: " << num2;
+    // }
+    // return 0 if false and 1 if true
+    cout << (num1 > num2)<<endl;
+}
 int main() {
-    my_number();
+    // my_number();
+    my_logic();
     return 0;
 } 
